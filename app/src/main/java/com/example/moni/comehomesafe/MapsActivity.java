0d6bacf6347f703d204a868176e6d623b5e0a43f
M0Nl <monika.silber@stud.uni-regensburg.de>
2016-09-04 16:51:44 +0200

    MapsActivity aktualisiert

--- app/src/main/java/com/example/moni/comehomesafe/MapsActivity.java
+++ app/src/main/java/com/example/moni/comehomesafe/MapsActivity.java
@@ -1,7 +1,13 @@
 package com.example.moni.comehomesafe;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,7 +16,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity
+        implements OnMapReadyCallback, View.OnClickListener {
 
     private GoogleMap mMap;
     private static final int LATITUDE_GERMANY = 51;
@@ -24,6 +31,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        initButtons();
+    }
+
+    private void initButtons() {
+        ImageButton btnCurrentLocation = (ImageButton) findViewById(R.id.button_gps_location);
+        btnCurrentLocation.setOnClickListener(this);
+        ImageButton btnDestination = (ImageButton) findViewById(R.id.button_destination);
+        btnDestination.setOnClickListener(this);
+        ImageButton btnAddCompanion = (ImageButton) findViewById(R.id.button_add_companion);
+        btnAddCompanion.setOnClickListener(this);
+        Button btnStartNavigation = (Button) findViewById(R.id.button_start_navigation);
+        btnStartNavigation.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_gps_location:
+                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                mMap.setMyLocationEnabled(true); //zum orten
+
+            case R.id.button_destination:
+                Intent intentDestination = new Intent(this, SelectDestinationActivity.class);
+                startActivity(intentDestination);
+
+            case R.id.button_add_companion:
+                Intent intentAddComp = new Intent(this, AddCompanionActivity.class);
+                startActivity(intentAddComp);
+
+            case R.id.button_start_navigation:
+                Intent intentStartNav = new Intent(this, NavigationActivity.class);
+                startActivity(intentStartNav);
+        }
+
     }
 
 
---
+++ app/src/main/java/com/example/moni/comehomesafe/NavigationActivity.java
@@ -0,0 +1,5 @@
+package com.example.moni.comehomesafe;
+
+
+public class NavigationActivity {
+}
