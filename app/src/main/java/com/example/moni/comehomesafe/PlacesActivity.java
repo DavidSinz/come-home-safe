9cd0e0371f77f542726455c118a1ee8e42d7a33e
DavisRauch <David.Rauch@stud.uni-regensburg.de>
2016-09-07 17:05:50 +0200

    Listview in PlacesActivity

--- app/src/main/java/com/example/moni/comehomesafe/PlacesActivity.java
+++ app/src/main/java/com/example/moni/comehomesafe/PlacesActivity.java
@@ -1,7 +1,50 @@
 package com.example.moni.comehomesafe;
 
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
 /**
  * Created by david_000 on 07.09.2016.
  */
-public class PlacesActivity {
+public class PlacesActivity extends FragmentActivity {
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        listView = (ListView) findViewById(R.id.places);
+
+        String[] values = new String[]{
+                "Zuhause",
+                "Schule",
+                "Uni",
+                "Arbeitsplatz"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // ListView Clicked item index
+                int itemPosition     = position;
+
+                // ListView Clicked item value
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+
+
+    }
 }
